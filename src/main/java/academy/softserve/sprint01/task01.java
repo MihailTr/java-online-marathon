8781b242000efe4c066e001a7f2409c1aca17c1c
MihailTr <tmyfly@gmail.com>
2020-09-11 08:12:57 +0300

    Finish task01 sprint01

--- src/main/java/academy/softserve/sprint01/task01.java
+++ src/main/java/academy/softserve/sprint01/task01.java
@@ -1,4 +1,15 @@
 package academy.softserve.sprint01;
-
+/**
+ * Create a Pet class with public fields name of type String and age of type int that are not initialized.
+ *
+ * * For correct passing of all tests don't use print and println methods in your code.
+ */
 public class task01 {
+
 }
+class Pet{
+    public String name;
+    public int age;
+}
+
+
