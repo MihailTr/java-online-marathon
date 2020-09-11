e389ee678bdaf48089fca52875c49d0a878d11e7
MihailTr <tmyfly@gmail.com>
2020-09-11 11:07:38 +0300

    Finish task04 sprint01.

---
+++ src/main/java/academy/softserve/sprint01/task04.java
@@ -0,0 +1,29 @@
+package academy.softserve.sprint01;
+
+/**
+ * Suppose we have the next class:
+ *
+ * Create a new version of the Circle class where the draw method will be overloaded four times:
+ *
+ *     The version without parameters.
+ *     Using the one parameter color of String type.
+ *     Using the one parameter scale of float type.
+ *     Using two parameters color and scale of String and float type.
+ *
+ * * For correct passing of all tests don't use print and println methods in your code.
+ */
+public class task04 {
+}
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public void draw(){}
+    public void draw(String color){}
+    public void draw(float scale){}
+    public void draw(String color, float scale){}
+}
