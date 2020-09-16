39cde057062efa4df576ff5a91291a80fe04bac8
MihailTr <tmyfly@gmail.com>
2020-09-16 13:32:14 +0300

    task05 sprint02. not finish.

---
+++ src/main/java/academy/softserve/sprint02/task05/MyUtils.java
@@ -0,0 +1,129 @@
+package academy.softserve.sprint02.task05;
+
+/**
+ * Create classes Square and Rectang with method (double getPerimeter()) for
+ * calculating of perimeter.
+ * Find solution for avoiding of duplicate code.
+ * Create a double sumPerimeter(List<?> firures) method of the MyUtils class
+ * to return a sum perimeters of all figures.
+ * For example, for a given list
+ * [[Square [width=4.00], Square [width=5.00], Rectang [height=2.00, width=3.00]]
+ * you should get 46
+ *
+ * import java.util.ArrayList;
+ * import java.util.List;
+ *
+ * class Rectang {
+ *     // Code
+ * }
+ * class Square {
+ *     // Code
+ * }
+ * public class MyUtils {
+ *     public double sumPerimeter(List<?> firures) {
+ *         // Code
+ *     }
+ * }
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Rectang {
+    // Code
+    private double height;
+    private double width;
+    private double sum;
+
+    public Rectang(){}
+
+    public Rectang(double width) {
+        this.width = width;
+    }
+
+    public Rectang(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getPerimeter(){
+        sum=0.0;
+        if (this.getClass().getSimpleName().equals("Rectang")) {
+           sum = (double) height * width;
+        }else if (this.getClass().getSimpleName().equals("Square")){
+           sum = (double) width * width;
+        }
+        return sum;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
+
+class Square extends Rectang {
+    // Code
+
+    public Square() {
+    }
+
+    public Square(double width) {
+        super(width);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
+public class MyUtils {
+    public double sumPerimeter(List<?> firures) {
+        // Code
+        double sumPer = 0.0;
+        if (firures == null) {
+            return 0.0;
+        }
+        if (firures.size() == 0) {
+            return 0.0;
+        } else {
+            for (Object o : firures) {
+                if (o.getClass().getSimpleName().equals("Rectang")) {
+                    sumPer = sumPer + ((Rectang) o).getPerimeter();
+                } else {
+                    sumPer = sumPer + ((Square) o).getPerimeter();
+                }
+            }
+            return sumPer;
+        }
+    }
+}
+
+
+class task05 {
+    public static void main(String[] args) {
+//        Rectang rectang = new Rectang(2, 5);
+        Square square = new Square(4);
+//        Square square1 = new Square(4.5);
+//        System.out.println(rectang.getPerimeter());
+        System.out.println(square.getPerimeter());
+//        System.out.println(square1.getPerimeter());
+        List<Object> firuresTest = new ArrayList<>();
+        firuresTest.add(new Square (4.00));
+        firuresTest.add(new Square (5.00));
+        firuresTest.add(new Rectang (2.00, 3.00));
+        System.out.println(new MyUtils().sumPerimeter(firuresTest));
+
+    }
+}
